2aaa4c8f7314c11f5a5dd45d8ac4f257c4cab2b7
bzanella <brunno.zanella@gmail.com>
2023-06-08 22:18:34 -0300

    tratativas

--- src/main/java/br/edu/unidep/interdisciplinar/repository/PersonRepository.java
+++ src/main/java/br/edu/unidep/interdisciplinar/repository/PersonRepository.java
@@ -2,17 +2,10 @@ package br.edu.unidep.interdisciplinar.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import br.edu.unidep.interdisciplinar.entity.Person;
 
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
-    @Procedure(name = "pr_delete_person")
-    void deletarPessoa(@Param("id_pessoa_in") Long id);
 
 }
