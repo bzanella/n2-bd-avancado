95b54b0ec23024946fb5ad2b1184448e41d87180
bzanella <brunno.zanella@gmail.com>
2023-06-08 22:29:22 -0300

    finalizacao

--- src/main/java/br/edu/unidep/interdisciplinar/controller/PersonController.java
+++ src/main/java/br/edu/unidep/interdisciplinar/controller/PersonController.java
@@ -1,8 +1,8 @@
 package br.edu.unidep.interdisciplinar.controller;
 
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,19 +17,39 @@ public class PersonController {
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/deletar/{id}")
-    public ResponseEntity<String> deletarPessoa(@PathVariable Long id, @RequestParam(name = "confirmar", defaultValue = "true") boolean confirmar) {
+    public ResponseEntity<String> deletarPessoa(
+      @PathVariable Long id,
+      @RequestParam(name = "confirmar", defaultValue = "true") boolean confirmar) {
+
         if (confirmar) {
             String sql = "CALL pr_delete_person(?)";
-            jdbcTemplate.update(sql, id);
-            return ResponseEntity.ok("Registro deletado com sucesso!");
+            int affectedRows = jdbcTemplate.update(sql, id);
+            if (affectedRows > 0) {
+                return ResponseEntity.ok("Registro deletado com sucesso!");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Registro não encontrado!");
+            }
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao deletar registro!");
         }
     }
 
+
     @GetMapping("/listar/{id}")
-    public Map<String, Object> getPessoaById(@PathVariable Long id) {
-        String sql = "SELECT fn_list_person_by_id(?)";
-        return jdbcTemplate.queryForMap(sql, id);
-    }
+    public ResponseEntity<Object> getPessoaById(@PathVariable Long id) {
+    String sql = "SELECT fn_list_person_by_id(?)";
+
+        try {
+            Map<String, Object> resultMap = jdbcTemplate.queryForMap(sql, id);
+            return ResponseEntity.ok(resultMap);
+        } catch (EmptyResultDataAccessException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa não encontrada");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar a solicitação");
+        }
+
+
+
+}
+
 }
