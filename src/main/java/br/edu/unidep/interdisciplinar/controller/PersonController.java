cdc6360874014de455f345e56a20352f357430f7
bzanella <brunno.zanella@gmail.com>
2023-06-08 21:32:44 -0300

    subindo projeto

--- src/main/java/br/edu/unidep/interdisciplinar/controller/PersonController.java
+++ src/main/java/br/edu/unidep/interdisciplinar/controller/PersonController.java
@@ -3,6 +3,8 @@ package br.edu.unidep.interdisciplinar.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,15 +17,13 @@ public class PersonController {
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/deletar/{id}")
-    public void deletarPessoa(@PathVariable Long id, @RequestParam(name = "confirmar", defaultValue = "true") boolean confirmar) {
+    public ResponseEntity<String> deletarPessoa(@PathVariable Long id, @RequestParam(name = "confirmar", defaultValue = "true") boolean confirmar) {
         if (confirmar) {
             String sql = "CALL pr_delete_person(?)";
             jdbcTemplate.update(sql, id);
-            System.out.println("Registro deletado com sucesso!");
-            
+            return ResponseEntity.ok("Registro deletado com sucesso!");
         } else {
-            System.out.println("Erro ao deletar registro!");
-
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao deletar registro!");
         }
     }
 
